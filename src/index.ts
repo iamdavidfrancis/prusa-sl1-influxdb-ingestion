05046475495cac7917312a4f11235efc156e2701
David Francis <david@iamdavidfrancis.com>
2020-08-29 19:59:20 -0700

    Have the app reconnect to the SL1 when the connection closes.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import WebsocketConnection from './webclient';
 import InfluxClient from './influx-client';
+import { start } from 'repl';
 
 class Main {
     constructor() {
+        
+    }
+
+    public async start() {
         const url = process.env.PRINTER_HOST;
 
         if (!url) {
@@ -14,8 +19,40 @@ class Main {
 
         const influxClient = new InfluxClient(url);
         
-        connection.initialize(url, influxClient);
+        await this.loop(() => connection.initialize(url, influxClient));
+    } 
+
+    private async loop(func: () => Promise<void>) {
+        const delay = (milliseconds) => {
+            return new Promise(res => {
+                setTimeout(res, milliseconds);
+            })
+        }
+        
+        const maxBackoff = 10 * 60 * 1000; // 10 min
+        const backoff = 100; 
+
+        while(true) {
+            try {
+                await func();
+                backoff = 100;
+            } catch (err) {
+                console.error(err);
+
+                // Every time we fail, back off a little longer.
+                // Delay amount resets on a success. (AKA connection succeeded then closed.)
+                await delay(Math.min(backoff, maxBackoff));
+                backoff *= 2;
+            }
+            
+        }
     }
+
+
+    
 }
 
 const app = new Main();
+app.start()
+    .then(() => console.log("Main start method ended. Weird."))
+    .catch((err) => console.error(`Main start method ended with an error: ${JSON.stringify(err, null, '\t')}`))
